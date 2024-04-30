352ded65e5db2757ae5dcde3ecf9509eb9a13e8b
CarlotaLaveron <carlota.laveronvilas@usp.ceu.es>
2024-04-30 12:36:43 +0200

    jr

--- src/PhysioClinicPOJOs/User.java
+++ src/PhysioClinicPOJOs/User.java
@@ -5,10 +5,12 @@ import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
+import javax.persistence.TableGenerator;
 
 
 @Entity
