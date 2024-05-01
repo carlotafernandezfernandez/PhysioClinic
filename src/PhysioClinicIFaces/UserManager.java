b4480e505d3136b8cc3484a3db85b504a41e1a09
CarlotaLaveron <carlota.laveronvilas@usp.ceu.es>
2024-05-01 14:01:46 +0200

    d

---
+++ src/PhysioClinicIFaces/UserManager.java
@@ -0,0 +1,18 @@
+package PhysioClinicIFaces;
+
+import java.util.List;
+import PhysioClinicPOJOs.Role;
+import PhysioClinicPOJOs.User;
+
+public interface UserManager {
+	
+	public void connect();
+	public void disconnect();
+	public void newRole(Role r);
+	public void newUser(User u);
+	public List<Role> getRoles();
+	public Role getRole(Integer id);
+	public User getUser(String email);
+	public User checkPassword(String email, String pass);
+
+}
