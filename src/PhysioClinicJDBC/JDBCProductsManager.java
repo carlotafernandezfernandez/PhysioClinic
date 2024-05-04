5548eeb5c4cd57f6b80c101382a8176f319850ec
CarlotaLaveron <carlota.laveronvilas@usp.ceu.es>
2024-05-04 11:47:14 +0200

    d

---
+++ src/PhysioClinicJDBC/JDBCProductsManager.java
@@ -0,0 +1,33 @@
+package PhysioClinicJDBC;
+
+import java.sql.PreparedStatement;
+
+import PhysioClinicIFaces.ProductsManager;
+
+public class JDBCProductsManager implements ProductsManager{
+	
+	private JDBCManager manager;
+	
+	public JDBCProductsManager (JDBCManager m) {
+		this.manager = m;
+	}
+
+	@Override
+	public void changeProductNAvailable(int product_id, int n_available) {
+		// TODO Auto-generated method stub
+		try {
+			String sql = "UPDATE products SET n_available= ? WHERE prod_id= ?;";
+			PreparedStatement prep = manager.getConnection().prepareStatement(sql);
+			
+			prep.setFloat(1, n_available);
+			prep.setInt(2, product_id);
+			
+			prep.executeQuery();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			
+		}
+	}
+		
+}
